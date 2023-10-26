a09d11d06c3433c5dc973c335b19aa2898099346
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2023-10-26 16:00:07 +0200

    Added getLcoation, getWeather

--- rxjs-fetch/main.js
+++ rxjs-fetch/main.js
@@ -11,15 +11,31 @@ const ip$ = fromFetch('https://api.ipify.org/').pipe(
 );
 
 function getLocation(ip) {
-const location$ = fromFetch('https://api.ipgeolocation.io/ipgeo?apiKey='+API_KEY+'&ip='+ip)
-.pipe(
-    switchMap(response=>response.json()),
-    tap(console.info),
-    map(ret=>({lat: ret.latitude, lng: ret.longitude})),
-)
-return location$;
+    const location$ = fromFetch('https://api.ipgeolocation.io/ipgeo?apiKey='+API_KEY+'&ip='+ip)
+    .pipe(
+        switchMap(response=>response.json()),
+        tap(console.info),
+        map(obj=>({lat: obj.latitude, lng: obj.longitude})),
+        tap(location=>{
+            document.getElementById('location').innerText=JSON.stringify(location);
+        }),
+    )
+    return location$;
+}
+
+function getWeather(location) {
+    const weather$ = fromFetch('https://api.open-meteo.com/v1/forecast?latitude='+location.lat+'&longitude='+location.lng+'&hourly=temperature_2m')
+    .pipe(
+        tap(console.log),
+        switchMap(ret=>ret.json()),
+        tap(weather=>{
+            document.getElementById('weather').innerText = JSON.stringify(weather);
+        }),
+    );
+    return weather$;
 }
 
 ip$.pipe(
     switchMap(getLocation),
+    switchMap(getWeather),
 ).subscribe(console.log);
